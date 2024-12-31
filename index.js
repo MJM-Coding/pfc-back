444937c96e003b608c039f58dfc7ad8a9d8056f0
Jessica_Moscato <j.mos@live.fr>
2024-12-31 10:28:56 +0100

    vercel.json taille des image

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.options("*", cors()); // Gère les requêtes préliminaires
 
 
 // Middleware pour traiter le JSON
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Middleware pour servir des fichiers statiques (CSS, images, etc.)
 app.use(express.static(path.join(__dirname, "public")));
